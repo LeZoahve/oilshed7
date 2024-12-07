91d90ff1a59ab8dfefaa58fa1aa16316625d563a
LeZoahve <michael.bouilloux.pro@gmail.com>
2024-12-07 15:38:44 +0100

    [Depot] Base code files

---
+++ oilshed7/Source/oilshed7/oilshed7.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "oilshed7.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, oilshed7, "oilshed7" );
