722fab35fe111c683288f7078373384a9e2136ac
LeZoahve <michael.bouilloux.pro@gmail.com>
2025-06-07 21:24:54 +0200

    [0] Time to get serious on this repo, bebouchou. Reseeeeeeet

--- oilshed7/Source/oilshed7/oilshed7.cpp
+++
@@ -1,6 +0,0 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
-
-#include "oilshed7.h"
-#include "Modules/ModuleManager.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, oilshed7, "oilshed7" );
